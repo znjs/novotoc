c456677f04433b8ef380af6d2a34e24132ceb32b
znjs <jasrikar@gmail.com>
2022-07-27 02:52:53 +0530

    UI: layouting UI components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Navbar, PageOptions, SubSections } from "./components";
 
 function App() {
-  return <div className="underline">hello world</div>;
+  return (
+    <div className="">
+      <div className="w-full h-[35%] bg-blue-600 absolute -z-10"></div>
+      <main className="max-w-[1140px] mx-auto flex flex-col">
+        <Navbar />
+        <p className="text-white text-2xl font-medium text-center my-4">
+          Products
+        </p>
+        <PageOptions />
+        <SubSections />
+      </main>
+    </div>
+  );
 }
 
 export default App;
